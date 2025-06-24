0820474d0dd3f94e87ded1afeb9a74076668ef23
Wok <woctezuma@users.noreply.github.com>
2025-06-24 16:57:22 +0200

    Add functions

--- update.py
+++ update.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 
 import cloudscraper
 
@@ -9,16 +10,93 @@ BASE_URL = f"{HOST}{ENDPOINT}"
 PLATFORMS = ["android", "ios"]
 START_INDEX = 0
 
-PARAMS = {
-    "count": "10",
-    "country": "US",
-    "locale": "en",
-    "platform": PLATFORMS[0],
-    "start": START_INDEX,
-    "store": "EGS",
-}
 
-scraper = cloudscraper.create_scraper()
-response = scraper.get(url=BASE_URL, params=PARAMS)
-text = response.content.decode("utf-8")
-parsed_data = json.loads(text)
+def get_default_params(platform: str, start_index: int = START_INDEX) -> dict[str, str]:
+    return {
+        "count": "10",
+        "country": "FR",
+        "locale": "en",
+        "platform": platform,
+        "start": str(start_index),
+        "store": "EGS",
+    }
+
+
+def save_data_for_platform(data: list, platform: str) -> None:
+    with Path(f"data/{platform}.json").open("w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4)
+
+
+def fetch_data_for_platform_and_start_index(
+    platform: str,
+    start_index: int,
+    scraper: cloudscraper.CloudScraper | None = None,
+) -> tuple[list, dict]:
+    if scraper is None:
+        scraper = cloudscraper.create_scraper()
+
+    params = get_default_params(platform, start_index)
+    response = scraper.get(url=BASE_URL, params=params)
+    text = response.content.decode("utf-8")
+    parsed_response = json.loads(text)
+
+    return parsed_response["data"], parsed_response["paging"]
+
+
+def fetch_data_for_platform(
+    platform: str,
+    scraper: cloudscraper.CloudScraper | None = None,
+    *,
+    save_to_disk: bool = True,
+) -> list:
+    if scraper is None:
+        scraper = cloudscraper.create_scraper()
+
+    start_index = START_INDEX
+    print(f"Fetching data for {platform} starting at index {start_index}")
+    data, paging = fetch_data_for_platform_and_start_index(
+        platform,
+        start_index=start_index,
+        scraper=scraper,
+    )
+
+    for start_index in range(
+        paging["start"] + paging["count"],
+        paging["total"],
+        paging["count"],
+    ):
+        print(f"Fetching data for {platform} starting at index {start_index}")
+        new_data, _ = fetch_data_for_platform_and_start_index(
+            platform,
+            start_index=start_index,
+            scraper=scraper,
+        )
+        data.extend(new_data)
+
+    if save_to_disk:
+        print(f"Saving data for {platform} to disk. Total items: {len(data)}")
+        save_data_for_platform(data, platform)
+
+    return data
+
+
+def fetch_data_for_every_platform(*, save_to_disk: bool = True) -> list:
+    scraper = cloudscraper.create_scraper()
+    all_data = []
+    for platform in PLATFORMS:
+        new_data = fetch_data_for_platform(platform, scraper)
+        all_data.extend(new_data)
+
+    if save_to_disk:
+        print("Saving all data to disk. Total items:", len(all_data))
+        save_data_for_platform(all_data, "all_platforms")
+
+    return all_data
+
+
+def main() -> None:
+    fetch_data_for_every_platform()
+
+
+if __name__ == "__main__":
+    main()
