efe13dadc48e826b4b873db7ce9b64fe9e5fb1e1
Wok <woctezuma@users.noreply.github.com>
2025-06-27 01:37:44 +0200

    Add a .sh script to update data

---
+++ update.sh
@@ -0,0 +1,14 @@
+python prepare_webhooks.py
+
+python update.py
+
+git add data/android.json
+git commit -m "🤖 Update JSON data for Android"
+
+git add data/ios.json
+git commit -m "🍏 Update JSON data for iOS"
+
+git add data/mobile_data.json
+git commit -m "🗃️ Update JSON data snapshot"
+
+git push
