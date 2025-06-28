fead5316b49798d98b43d854df01bd62831e7a5f
Wok <woctezuma@users.noreply.github.com>
2025-06-28 15:12:32 +0200

    Prefix git commit messages with fixup

--- update.sh
+++ update.sh
@@ -5,12 +5,12 @@ python update.py
 python compare_data.py
 
 git add data/android.json
-git commit -m "🤖 Update JSON data for Android"
+git commit -m "fixup! 🤖 Update JSON data for Android"
 
 git add data/ios.json
-git commit -m "🍏 Update JSON data for iOS"
+git commit -m "fixup! 🍏 Update JSON data for iOS"
 
 git add data/mobile_data.json
-git commit -m "🗃️ Update JSON data snapshot"
+git commit -m "fixup! 🗃️ Update JSON data snapshot"
 
 git push
